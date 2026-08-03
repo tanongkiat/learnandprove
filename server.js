59a78e4f24606ea41ca0d886bb655e9f9052074e
boyd <tanongkiat@users.noreply.github.com>
2026-08-03 20:54:35 +0700

    Add circle problem canvas drawing + route /training/circle-problem

--- server.js
+++ server.js
@@ -21,6 +21,7 @@ app.get('/', (req, res) => res.sendFile(path.join(VIEWS, 'home.html')));
 
 app.get('/training', (req, res) => res.sendFile(path.join(VIEWS, 'training.html')));
 app.get('/training/study-plan', (req, res) => res.sendFile(path.join(VIEWS, 'study-plan.html')));
+app.get('/training/circle-problem', (req, res) => res.sendFile(path.join(VIEWS, 'circle-problem.html')));
 
 app.get('/practice', (req, res) => res.sendFile(path.join(VIEWS, 'practice.html')));
 
